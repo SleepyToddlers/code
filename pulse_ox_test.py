53183e4080b5ee280023290781e9891058dee5ba
David Sniecinski <dmsnie@umich.edu>
2018-01-28 22:13:03 -0500

    -

--- pulse_ox_test.py
+++ pulse_ox_test.py
@@ -55,8 +55,8 @@ while True:
     if len(red_filter) > 500:
         # Make calculations
         print(len(ir_filter))
-        AC_RED = math.sqrt(sum([i**2 for i in red_filter]/len(red_filter)))
-        AC_IR  = math.sqrt(sum([i**2 for i in ir_filter]/len(ir_filter)))
+        AC_RED = math.sqrt(sum([i**2 for i in red_filter])/len(red_filter))
+        AC_IR  = math.sqrt(sum([i**2 for i in ir_filter])/len(ir_filter))
 
         SPO2 = 110 - 25*math.log10(AC_RED)/math.log10(AC_IR)
 
