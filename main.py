f4f4ca19ecdd1bc9839d51bf99cdcece381d7590
SleepyToddlers <fqiu10@gmail.com>
2018-03-14 12:31:01 -0400

    fixed loop

--- main.py
+++ main.py
@@ -31,13 +31,14 @@ file = open('ted_shirt_verification.txt', 'w')
 file.write(start_time)
 
 # Get the data
-while(start_time < end_time):
-    a,b,c,d = adc.read_data()
-    hr.append(str(a))
-    nas_temp.append(str(b))
-    belt.append(str(c))
-    mic.append(str(d))
-    sleep_position.append(str(imu.sleep_position()))
+while(time.time() < end_time):
+	for i in range(0,5000)
+		a,b,c,d = adc.read_data()
+		hr.append(str(a))
+		nas_temp.append(str(b))
+		belt.append(str(c))
+		mic.append(str(d))
+		sleep_position.append(str(imu.sleep_position()))
 
 
 file.write(str(time.time() - start_time))
